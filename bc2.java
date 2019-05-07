45cd0290dd31218bb9bf9bb5ac47b091e4802ba7
Anish Mohan <anishmohant@gmail.com>
2019-05-07 00:40:53 -0400

    other programs

---
+++ bc2.java
@@ -0,0 +1,14 @@
+class bc2
+{
+	public static void main(String  args[]) 
+	{
+		int x = 1;
+		while (x<10) 
+		{
+			if (x > 3) // wont enter here because of not satisfying condition 
+			{
+				System.out.println("big x");
+			}
+		}
+	}
+}
---
+++ bc3.java
@@ -0,0 +1,15 @@
+class bc3
+{
+	public static void main(String args[])
+	{
+		int x = 5;
+		while (x>5)
+		{
+			x=x-1;
+			if ( x <3) 
+			{
+				System.out.println("small x");
+			}
+		}
+	}
+}
