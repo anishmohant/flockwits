b06cad1c02c7bc45585f6f020e30c65ad8a33ce0
Anish Mohan <anishmohant@gmail.com>
2019-05-08 03:41:35 -0400

    changes

--- tictactoe.java
+++ tictactoe.java
@@ -1,12 +1,13 @@
 import java.io.*;
 import java.util.Scanner;
-class tictactoe{
+public class tictactoe{
 	int [][] a = new int [3][3];
+	boolean arrayFull=false;
 
 	public static void main (String args [])
 	{
 		
-		System.out.println("TicTackToe");
+		System.out.println("TicTacToe");
 		tictactoe t= new tictactoe();
 		/*for(int i=0;i<3;i++)
 		{
@@ -17,6 +18,22 @@ class tictactoe{
 		}*/
 
 		t.display();
+
+	}
+	void boolean arrayFull()
+	{
+		int af;
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				if(a[i][j]>0)
+				{
+					
+					return true	
+				}
+			}
+		}
 
 	}
 
@@ -42,17 +59,40 @@ public class tictactoecore
 {
 	Player p1;
 	Player p2;
+
 	void startgame()
 	{
-		//while the matrix in not full
-		tictactoe t= new tictactoe();
-		p1.strike();
-		p2.strike();			
+		p1.symbol=1;
+		p2.symbol=8;
+		while(arrayFull!=true)
+		{
+			tictactoe t= new tictactoe();
+			p1.strike();
+			if(t.a[p1.row][p1.column]==0)
+			{
+				t.a[p1.row][p1.column]=p1.symbol;
+			}
+			else
+			{
+				System.out.print("That cell already marked");
+			}
+			
+			p2.strike();
+			if(t.a[p2.row][p2.column]==0)
+			{
+				t.a[p2.row][p2.column]=p2.symbol;
+			}
+			else
+			{
+				System.out.print("That cell already marked");
+			}
+			
+		}			
 
 	}
 	public void display()
 	{
-		System.out.println(" a  b  c");
+		System.out.println(" 1  2  3");
 		for(int i=0;i<3;i++)
 		{
 			System.out.println("");
